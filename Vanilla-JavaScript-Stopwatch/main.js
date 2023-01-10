4df6df1c2cb4bbb9450837e134e08103466d7a9a
Khalid Najmeddine <knajm574@gmail.com>
2023-01-10 12:11:10 -0700

    feat: html layout of the Stopwatch

--- Vanilla-JavaScript-Stopwatch/main.js
+++ Vanilla-JavaScript-Stopwatch/main.js
@@ -1,8 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector().innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
